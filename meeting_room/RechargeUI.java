ce0f7923a6a6a608f3f62c788270a5bbf4b1c98a
sth <calmest_@naver.com>
2024-02-07 11:36:01 +0900

    logo update

--- meeting_room/RechargeUI.java
+++ meeting_room/RechargeUI.java
@@ -32,39 +32,37 @@ public class RechargeUI extends JFrame {
 	
 	public RechargeUI(){
 		setTitle("충전");
-		setSize(800, 600);
+		setSize(800, 800);
 		
 		//컨텐츠 패널의 객체 메소드 호출
 		Container c = getContentPane();
 		c.setLayout(null); //컨텐츠 패널 초기
 		
-		// 이미지 크기에 맞게 JLabel 크기 조절 
-		Dimension imageSize = new Dimension(icon.getIconWidth(), icon.getIconHeight());
-		logo_lb.setPreferredSize(imageSize);
-		       
-		// 이미지를 수평으로 중앙, 사용자가 정한 세로 좌표에 위치시킴
-		int x = (getWidth() - imageSize.width) / 2;
-		logo_lb.setBounds(x, 35, imageSize.width, imageSize.height);
+		// 로고 추가
+        ImageIcon logoIcon = new ImageIcon("meeting_room/logo.jpg");
+        JLabel logoLabel = new JLabel(logoIcon);
+        logoLabel.setBounds(45, 50, 700, 270);
+        c.add(logoLabel);
 		
 		Font font = new Font("Dialog", Font.PLAIN, 18);
 		
-		charge_1000_btn.setBounds(30, 210, 120, 70);
-		charge_5000_btn.setBounds(180, 210, 120, 70);
-		charge_10000_btn.setBounds(330, 210, 120, 70);
-		charge_30000_btn.setBounds(480, 210, 120, 70);
-		charge_50000_btn.setBounds(630, 210, 120, 70);
+		charge_1000_btn.setBounds(30, 410, 120, 70);
+		charge_5000_btn.setBounds(180, 410, 120, 70);
+		charge_10000_btn.setBounds(330, 410, 120, 70);
+		charge_30000_btn.setBounds(480, 410, 120, 70);
+		charge_50000_btn.setBounds(630, 410, 120, 70);
 		
-		recharge_amount_lb.setBounds(250, 330, 120, 35);
-		recharge_value_tf.setBounds(350, 330, 120, 35);
+		recharge_amount_lb.setBounds(250, 530, 120, 35);
+		recharge_value_tf.setBounds(350, 530, 120, 35);
 		// 텍스트 필드를 우측으로 정렬
 		recharge_value_tf.setHorizontalAlignment(SwingConstants.RIGHT);
-		won_lb.setBounds(485, 330, 50, 35);
+		won_lb.setBounds(485, 530, 50, 35);
 		
 		recharge_amount_lb.setFont(font);
 		won_lb.setFont(font);
 		
-		recharge_btn.setBounds(250 , 400, 120, 70);
-		cancel_btn.setBounds(400, 400, 120, 70);
+		recharge_btn.setBounds(250, 600, 120, 70);
+		cancel_btn.setBounds(400, 600, 120, 70);
 		
 		c.add(logo_lb);
 		c.add(charge_1000_btn);
